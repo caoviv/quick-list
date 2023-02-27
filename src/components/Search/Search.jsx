0b7a34ec768ed383b2489cc8ce7b9ce823b71c5d
VivianC <viviancao58@gmail.com>
2023-02-27 21:49:49 +1100

    create basic search method for recipes

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Recipes } from "../../data/recipes";
+function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Pick a recipe..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <ul className="recipe-list">
+        {Recipes.filter((recipe) =>
+          recipe.name.toLowerCase().includes(query)
+        ).map((recipe) => (
+          <li key={recipe.id} className="recipe">
+            {recipe.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Search;
