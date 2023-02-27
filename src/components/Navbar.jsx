734d2832fd8a7acbbab132304dec451bf895bd1b
VivianC <viviancao58@gmail.com>
2023-02-27 16:10:35 +1100

    create simple navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./NavbarStyles.css";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Quick List
+      </a>
+      {/* <ul>
+        <li>
+          <a href="/" className="dark-light">
+            D/L
+          </a>
+        </li>
+      </ul> */}
+    </nav>
+  );
+}
+
+export default Navbar;
