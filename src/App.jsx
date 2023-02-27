8ecc508880eaa9ccada9d427b1a4b972cdd96515
VivianC <viviancao58@gmail.com>
2023-02-27 21:16:54 +1100

    import Recipes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import './variables.css'
 import Navbar from "./components/Navbar/Navbar";
+import Recipes from "./components/Recipes/Recipes";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Recipes />
     </div>
   );
 }
