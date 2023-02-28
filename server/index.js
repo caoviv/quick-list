b425b5074fb20318adfecf29cf1cce08314b18be
VivianC <viviancao58@gmail.com>
2023-02-28 14:43:35 +1100

    create initial / GET method

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
-const app = require("express")();
-const PORT = 8080;
+import express from "express";
+import cors from "cors";
+import { Recipes } from "./data/recipes.js";
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+app.get("/", (req, res) => {
+  res.json(Recipes);
+});
