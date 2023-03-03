49a132fcfc2802e89d5b2aa61f36e3438d024dc4
VivianC <viviancao58@gmail.com>
2023-03-03 15:28:01 +1100

    add comments for readability

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -3,15 +3,21 @@ import { Recipes } from "../../data/recipes";
 import axios from "axios";
 
 function Search() {
+  // states
   const [query, setQuery] = useState("");
   const [data, setData] = useState([]);
 
+  // effect after render
   useEffect(() => {
+    // fetch recipe data function
     const fetchRecipes = async () => {
+      // use axios to make a get request for recipes data from local server 5000
       const res = await axios.get("http://localhost:5000");
+      // response is then set to data status
       setData(res.data);
     };
     fetchRecipes();
+    // empty array - runs only on the first render
   }, []);
 
   return (
@@ -20,12 +26,18 @@ function Search() {
         type="text"
         placeholder="Pick a recipe..."
         className="search"
+        // set value of the text input to data status for filter function 
         onChange={(e) => setQuery(e.target.value)}
       />
       <ul className="recipe-list">
+        {/* use data status  */}
         {data
+        // filter data according to the query status 
+        // recipe name is set to lower case to avoid case sensitivity
           .filter((recipe) => recipe.name.toLowerCase().includes(query))
+          // iterate over recipes array using .map 
           .map((recipe) => (
+            // output makes a list of recipes from the data fetched
             <li key={recipe.id} className="recipe">
               {recipe.name}
             </li>
