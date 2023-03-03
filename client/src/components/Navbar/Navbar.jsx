f6500ba95b5b0d3617816990ba3b7e3c342c264d
VivianC <viviancao58@gmail.com>
2023-03-03 15:27:44 +1100

    add comments for readability

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ function Navbar() {
   return (
     <header>
       <nav>
+        {/* site title with link to home page */}
         <a href="/" className="site-title">
           Quick List
         </a>
