5d264af6b472ecfa1b190744fe8424bf39e04f04
VivianC <viviancao58@gmail.com>
2023-03-03 14:53:25 +1100

    add fetch data from server fn

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Recipes } from "../../data/recipes";
+import axios from "axios";
+
 function Search() {
   const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const res = await axios.get("http://localhost:5000");
+      setData(res.data);
+    };
+    fetchRecipes();
+  }, []);
 
   return (
     <div>
       <input
         type="text"
-        placeholder="Select an option"
+        placeholder="Pick a recipe..."
         className="search"
         onChange={(e) => setQuery(e.target.value)}
       />
       <ul className="recipe-list">
-        {Recipes.filter((recipe) =>
-          recipe.name.toLowerCase().includes(query)
-        ).map((recipe) => (
-          <li key={recipe.id} className="recipe">
-            {recipe.name}
-          </li>
-        ))}
+        {data
+          .filter((recipe) => recipe.name.toLowerCase().includes(query))
+          .map((recipe) => (
+            <li key={recipe.id} className="recipe">
+              {recipe.name}
+            </li>
+          ))}
       </ul>
     </div>
   );
