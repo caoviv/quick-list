dcac29c1f3021910193956845dad21c2367f1a34
VivianC <viviancao58@gmail.com>
2023-03-06 12:39:37 +1100

    initial set up of comp

---
+++ client/src/components/GroceryList/GroceryList.jsx
@@ -0,0 +1,12 @@
+// CSS Styles
+import "./GroceryListStyles.css";
+
+function GroceryList() {
+  return (
+    <div className="wrapper">
+      <h1>Grocery List</h1>
+    </div>
+  );
+}
+
+export default GroceryList;
