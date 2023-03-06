93c24ff1b89a6423f52381f5a109c018ee6a4969
VivianC <viviancao58@gmail.com>
2023-03-06 12:34:29 +1100

    import RecipeIngredients comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import "./variables.css";
 import Navbar from "./components/Navbar/Navbar";
 import RecipeSearch from "./components/RecipeSearch/RecipeSearch";
+import RecipeIngredients from "./components/RecipeIngredients/RecipeIngredients";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <RecipeSearch className="recipe-search-comp" />
+      <section className="main-section">
+        <RecipeSearch className="recipe-search-comp" />
+        <RecipeIngredients />
+      </section>
     </div>
   );
 }
