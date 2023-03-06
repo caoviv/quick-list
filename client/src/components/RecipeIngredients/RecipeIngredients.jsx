9d51616c67956dd8ca07bb2b25e025ebb40061ea
VivianC <viviancao58@gmail.com>
2023-03-06 12:34:09 +1100

    initial set up of comp

---
+++ client/src/components/RecipeIngredients/RecipeIngredients.jsx
@@ -0,0 +1,12 @@
+// CSS Styles
+import "./RecipeIngredientsStyles.css";
+
+function RecipeIngredients() {
+  return (
+    <div>
+      <h1>Ingredients</h1>
+    </div>
+  );
+}
+
+export default RecipeIngredients;
