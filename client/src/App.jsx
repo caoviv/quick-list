ca18079db2161eea55dd9c98c3d6d99d53b9f4f2
VivianC <viviancao58@gmail.com>
2023-03-09 21:05:52 +1100

    remove recipe ingredients comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <Navbar />
       <section className="main-section">
         <RecipeSearch className="recipe-search-comp" />
-        <RecipeIngredients />
         <GroceryList />
       </section>
     </div>
