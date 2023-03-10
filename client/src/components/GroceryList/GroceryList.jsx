e8e253c9a22b263254a008d98d4fab2c70c958f4
VivianC <viviancao58@gmail.com>
2023-03-10 17:07:40 +1100

    add fn - checked ingredients added to a list

--- client/src/components/GroceryList/GroceryList.jsx
+++ client/src/components/GroceryList/GroceryList.jsx
@@ -1,10 +1,15 @@
 // CSS Styles
 import "./GroceryListStyles.css";
 
-function GroceryList() {
+function GroceryList({ ingredients }) {
   return (
     <div className="wrapper">
       <h1>Grocery List</h1>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- client/src/components/RecipeIngredients/RecipeIngredients.jsx
+++ client/src/components/RecipeIngredients/RecipeIngredients.jsx
@@ -1,22 +1,53 @@
-import { Recipes } from "../../../data/recipes";
+import { useState } from "react";
+import GroceryList from "../GroceryList/GroceryList";
 
 // CSS Styles
 import "./RecipeIngredientsStyles.css";
 
 function RecipeIngredients({ recipe }) {
+  // states
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [viewGroceryList, setViewGroceryList] = useState(false);
+  const handleCheckBoxChange = (event) => {
+    // object destructuring to extract 'value' and 'checked' from checkbox element
+    const { value, checked } = event.target;
+    // when the checkbox is checked
+    if (checked) {
+      // add the ingredient checked to the selectedIngredient state
+      setSelectedIngredients([...selectedIngredients, value]);
+      // when the checkbox is unchecked
+    } else {
+      // remove the ingredient checked to the selectedIngredient state
+      setSelectedIngredients(
+        selectedIngredients.filter((ingredient) => ingredient !== value)
+      );
+    }
+  };
+
   return (
     <div class="module">
-      <h1>Ingredients</h1>
-      <h2>{recipe.name}</h2>
-      <ul className="ingredients-list">
-        {recipe.ingredients.map((ingredient, index) => (
-          <li>
-            <input type={"checkbox"} name={ingredient} key={index} />
-            <label for="ingredient">{ingredient}</label>
-          </li>
-        ))}
-      </ul>
-      {/* <button onClick={onClose}>Close</button> */}
+      <section>
+        <h1>Ingredients</h1>
+        <h2>{recipe.name}</h2>
+        <ul className="ingredients-list">
+          {recipe.ingredients.map((ingredient, index) => (
+            <li key={index}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={ingredient}
+                  onChange={handleCheckBoxChange}
+                />
+                {ingredient}
+              </label>
+            </li>
+          ))}
+        </ul>
+        {/* <button onClick={onClose}>Close</button> */}
+        <button onClick={() => setViewGroceryList(true)}>+ Add to list</button>
+      </section>
+      {/* conditional render GroceryList comp only when a recipe is clicked  */}
+      {viewGroceryList && <GroceryList ingredients={selectedIngredients} />}
     </div>
   );
 }
